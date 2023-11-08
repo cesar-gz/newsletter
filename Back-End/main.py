e6e775d487b541c5e1d170d27035025909137512
cesar-gz <cesarg7@csu.fullerton.edu>
2023-11-07 19:16:43 -0800

    fix for CORS error

--- Back-End/main.py
+++ Back-End/main.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
-from routes import router  
+from fastapi.middleware.cors import CORSMiddleware
+from routes import router
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:4200",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(router)
 if __name__ == "__main__":
